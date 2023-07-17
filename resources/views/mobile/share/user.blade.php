264abf917d41924866bf5e2abd9d1bbb73c3f6de
qingtian <1043791113@qq.com>
2023-07-17 10:49:06 +0800

    分享列表

--- resources/views/mobile/share/user.blade.php
+++ resources/views/mobile/share/user.blade.php
@@ -8,6 +8,10 @@
             <p>Convite nível 1{{--二级邀请--}}</p>
         </div>
 </div>
+
+{{--loading组件--}}
+@include('mobile.common.loading')
+
 <div class="yh_table">
     <table>
         <thead>
@@ -29,4 +33,33 @@
         @endforeach  
         </tbody>
     </table>
+    
+    <div style="width:100%;text-align:center;margin-top:1rem">
+        <button id="pg_load_more" page="0" onclick="loadInvites()"  style="color:#fff; font-size:14px;">{{--点击加载更多--}}Clique para carregar mais</button>
+    </div>
+
 </div>
+
+<script>
+    function loadInvites() {
+        let page = $('#pg_load_more').attr('page');
+          showLoading();
+          $.ajax({
+              url : "{{url('mobile/share/invites')}}",
+              type : 'GET',
+              data : {page: parseInt(page) + 1},
+              success : function (data) {
+                hideLoading();
+                $('#pg_load_more').attr('page', data.data.current_page);
+                data.data.data.forEach(element => {
+                  let itemGame = '<a><img _ngcontent-avh-c16="" gameid="'+element.id+'" class=" generic-background-image pg_game_go ng-star-inserted" src="'+element.icon+'" /></a>'
+                  $('#tab2_content_pgs').append(itemGame)
+                })
+              }
+          })
+    }
+    
+    $(document).ready(function() {
+            loadInvites();  
+    })
+</script>
