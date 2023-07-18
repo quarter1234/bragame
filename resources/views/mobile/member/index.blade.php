19e85bbbc630c254c4084e9ab2f6a4a784813092
qingtian <1043791113@qq.com>
2023-07-18 19:20:08 +0800

    test

--- resources/views/mobile/member/index.blade.php
+++ resources/views/mobile/member/index.blade.php
@@ -58,7 +58,14 @@
                     <jx-avatar _ngcontent-xfs-c1="" class="ng-tns-c1-1" _nghost-xfs-c6="">
                       <img _ngcontent-xfs-c6="" alt="" class="avatar-icon" src="{{$avatar}}" style="border-radius: 50%;"></jx-avatar>
                     <div _ngcontent-xfs-c1="" class="ng-tns-c1-1">
-                      <div _ngcontent-xfs-c1="" class="username">{{ $user['playername'] }} <p>VIP:{{ $user['svip'] }}</p><p> ID: {{ $user['uid'] }}</p>  </div>
+                      <div _ngcontent-xfs-c1="" class="username">{{ $user['playername'] }} 
+                        <p class="avatar_sub_title">VIP :  <span class="avatar_sub_title-span">{{ $user['svip'] }}</span></p>
+                        <p class="avatar_sub_title">I D :  <span class="avatar_sub_title-span">{{ $user['uid'] }}</span></p>  
+                        <style>
+                          .avatar_sub_title{font-size:1rem;line-height: 1.8rem;}
+                          .avatar_sub_title-span{margin-left:0.6rem;}
+                        </style>
+                      </div>
                       <!---->
                       </div>
                   </div>
