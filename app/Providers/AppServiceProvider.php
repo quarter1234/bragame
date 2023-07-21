1ccb8cbc7187cfbb92acd5e376eedb23c6a3f0a1
qingtian <1043791113@qq.com>
2023-07-21 19:17:38 +0800

    htts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        // 是否 HTTPS 环境
+        // IS_HTTPS=true
+        // HTTPS 访问
+        if(env('IS_HTTPS')){
+            URL::forceScheme('https');
+        }
     }
 }
