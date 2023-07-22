2a93126bfecf96a50f92636bea921ee931af11ac
qingtian <1043791113@qq.com>
2023-07-22 13:59:31 +0800

    支付地址

--- config/pay.php
+++ config/pay.php
@@ -1,8 +1,10 @@
 <?php
+
 return [
-    'payurl' => 'https://h5.condebet.com:84/#/pages/deposit/add/add', // 支付网关地址
-    'kyc' => 'https://h5.condebet.com:84/#/pages/audit/apply/apply', // kyc验证地址
-    'draw' => 'https://h5.condebet.com:84/#/pages/withdraw/index',
-    'transaction' => 'https://h5.condebet.com:84/#/pages/transaction/transaction',
-    'payment' => 'https://h5.condebet.com:84/#/pages/deposit/managepayment/managepayment',
+    
+    'payurl' => "https://{app('app.url')}:84/#/pages/deposit/add/add", // 支付网关地址
+    'kyc' => "https://{app('app.url')}:84/#/pages/audit/apply/apply", // kyc验证地址
+    'draw' => "https://{app('app.url')}:84/#/pages/withdraw/index",
+    'transaction' => "https://{app('app.url')}:84/#/pages/transaction/transaction",
+    'payment' => "https://{app('app.url')}:84/#/pages/deposit/managepayment/managepayment",
 ];
