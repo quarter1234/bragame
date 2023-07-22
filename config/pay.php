d47baf1caa2bfc605fbf37740666875878ca850c
qingtian <1043791113@qq.com>
2023-07-22 14:07:57 +0800

    test

--- config/pay.php
+++ config/pay.php
@@ -1,10 +1,10 @@
 <?php
-
+$baseUrl = app('app.url');
 return [
     
-    'payurl' => "https://".app('app.url').":84/#/pages/deposit/add/add", // 支付网关地址
-    'kyc' => "https://".app('app.url').":84/#/pages/audit/apply/apply", // kyc验证地址
-    'draw' => "https://".app('app.url').":84/#/pages/withdraw/index",
-    'transaction' => "https://".app('app.url').":84/#/pages/transaction/transaction",
-    'payment' => "https://".app('app.url').":84/#/pages/deposit/managepayment/managepayment",
+    'payurl' => "https://{$baseUrl}:84/#/pages/deposit/add/add", // 支付网关地址
+    'kyc' => "https://{$baseUrl}:84/#/pages/audit/apply/apply", // kyc验证地址
+    'draw' => "https://{$baseUrl}:84/#/pages/withdraw/index",
+    'transaction' => "https://{$baseUrl}:84/#/pages/transaction/transaction",
+    'payment' => "https://{$baseUrl}:84/#/pages/deposit/managepayment/managepayment",
 ];
