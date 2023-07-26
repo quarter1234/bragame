e4bd88e28f901be077c6300b704b5ed97f7b2766
qingtian <1043791113@qq.com>
2023-07-26 16:35:00 +0800

    test

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -26,5 +27,8 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        Auth::viaRemember(function () {
+            return null; // 返回 null 表示通过 remember_token 不进行自动登录        
+        });
     }
 }
