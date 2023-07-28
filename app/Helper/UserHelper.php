60473c650226408d253d50d65001bc025c40ce8c
qingtian <1043791113@qq.com>
2023-07-28 14:12:54 +0800

    test

--- app/Helper/UserHelper.php
+++ app/Helper/UserHelper.php
@@ -9,7 +9,7 @@ class UserHelper
 {
     public static function avatar($usericon)
     {
-        return ' https://wwv.condebet.com/bx_4/public/head/head_'.$usericon.'.png';
+        return ' https://wwv.condebet.com/bx_4/public/static/head/head_'.$usericon.'.png';
     }
 
     /**
