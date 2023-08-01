9a14bd1b3eceab91e750376d42ee0bd67f7dcba0
qingtian <1043791113@qq.com>
2023-08-01 10:34:23 +0800

    test

--- routes/mobile.php
+++ routes/mobile.php
@@ -14,9 +14,6 @@ use App\Http\Controllers\Mobile\ShareController;
 use App\Http\Controllers\Mobile\ShopController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', [IndexController::class, 'index'])->name('login');
-
 Route::group([
     'prefix' => 'mobile'
 ], function ($router) {
