7834261b444fa2a9e453a9ba785911dc509851d1
qingtian <1043791113@qq.com>
2023-08-01 10:27:13 +0800

    跨域

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -15,16 +15,10 @@ class CrossHttp
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);        
-        $response->header('Access-Control-Allow-Origin', '*');        
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');        
-        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');        
-        return $response;    
-
-        // return  $next($request)->header('Access-Control-Allow-Origin', '*')
-        //     ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN, X-REQUEST-WITH, ACCESSTOKEN')
-        //     ->header('Access-Control-Expose-Headers', 'Authorization, authenticated')
-        //     ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        //     ->header('Access-Control-Allow-Credentials', 'true');
+        return  $next($request)->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN, X-REQUEST-WITH, ACCESSTOKEN')
+            ->header('Access-Control-Expose-Headers', 'Authorization, authenticated')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+            ->header('Access-Control-Allow-Credentials', 'true');
     }
 }
