acadb5f922f9587f17cb79ecfd23f1f0e703c5b0
qingtian <1043791113@qq.com>
2023-08-01 10:37:34 +0800

    youhua

--- routes/mobile.php
+++ routes/mobile.php
@@ -14,6 +14,9 @@ use App\Http\Controllers\Mobile\ShareController;
 use App\Http\Controllers\Mobile\ShopController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', [IndexController::class, 'index'])->name('login');
+
 Route::group([
     'prefix' => 'mobile'
 ], function ($router) {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Mobile\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [IndexController::class])->name('login');
+
 
 
 
