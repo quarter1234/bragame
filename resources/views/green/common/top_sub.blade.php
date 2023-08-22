f527c8dd785bee79aaa9c916113695e428206445
xdr <170882208@qq.com>
2023-08-22 17:11:10 +0800

    顶部跳转

--- resources/views/green/common/top_sub.blade.php
+++ resources/views/green/common/top_sub.blade.php
@@ -6,7 +6,7 @@
         <span>R$</span>
         <span>{{$user['coin']}}</span>
         <div class="sx"><img src="https://wwv.condebet.com/bx_4/public/mobile/img/sx.png" /></div>
-        <div class="qb"><img onclick="location.href='{{ route("mobile.display", ["act" => "pay"]) }}'" src="../../mobile/img/qb.png" /></div>
+        <div class="qb"><img onclick="location.href='{{ url("mobile/pay/recharge") }}'" src="../../mobile/img/qb.png" /></div>
     </div>
 
 </div>
