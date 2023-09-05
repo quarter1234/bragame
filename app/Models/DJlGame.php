552c51a9a0f9df057e97c70df7ec195efaca3c4e
xdr <170882208@qq.com>
2023-09-05 19:37:13 +0800

    jili修改

--- app/Models/DJlGame.php
+++ app/Models/DJlGame.php
@@ -5,4 +5,10 @@ class DJlGame extends BaseMoel
 {
     public $timestamps = false;
     protected $table = 'd_jl_game';
+
+    public function getIconAttribute($value)
+    {
+        // return 'https://baxigame1.s3.sa-east-1.amazonaws.com/bx_4/public'.$value;
+        return config('app.url').':82/public' .  $value;
+    }
 }
