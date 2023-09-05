de28441ab8f7074b0d6c86902dd24f23cd343d8d
xdr <170882208@qq.com>
2023-09-05 19:47:36 +0800

    jili修改

--- app/Models/DJlGame.php
+++ app/Models/DJlGame.php
@@ -8,7 +8,7 @@ class DJlGame extends BaseMoel
 
     public function getIconAttribute($value)
     {
-        // return 'https://baxigame1.s3.sa-east-1.amazonaws.com/bx_4/public'.$value;
-        return config('app.url').':82' .  $value;
+        return 'https://baxigame1.s3.sa-east-1.amazonaws.com/bx_4/public'.$value;
+        // return config('app.url').':82' .  $value;
     }
 }
