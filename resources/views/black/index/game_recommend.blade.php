e670f412d3d6094be0a72c51553323dfd12a5d43
xdr <170882208@qq.com>
2023-09-08 17:35:47 +0800

    界面

--- resources/views/black/index/game_recommend.blade.php
+++ resources/views/black/index/game_recommend.blade.php
@@ -3,6 +3,9 @@
         <div class="game_qb_left">
            Popular
         </div>
+        <div class="game_qb_left" style="text-align:right;">
+            <a href="#navigations" style="text-decoration:none" onclick="myclick(9)">Ver tudo</a>
+        </div>
     </div>
     {{--游戏放这里 喜欢的--}}
     <div class="game_list rm">
@@ -14,6 +17,9 @@
     <div class="game_qb_title">
         <div class="game_qb_left">
           In-PG
+        </div>
+        <div class="game_qb_left" style="text-align:right;">
+            <a href="#navigations" style="text-decoration:none" onclick="myclick(2)">Ver tudo</a>
         </div>
     </div>
     {{--游戏放这里 PG--}}
@@ -27,6 +33,9 @@
         <div class="game_qb_left">
             In-PP
         </div>
+        <div class="game_qb_left" style="text-align:right;">
+            <a href="#navigations" style="text-decoration:none" onclick="myclick(1)">Ver tudo</a>
+        </div>
     </div>
     {{--游戏放这里--}}
     <div class="game_list">
@@ -39,6 +48,9 @@
     <div class="game_qb_title">
         <div class="game_qb_left">
            In-Tada
+        </div>
+        <div class="game_qb_left" style="text-align:right;">
+            <a href="#navigations" style="text-decoration:none" onclick="myclick(3)">Ver tudo</a>
         </div>
     </div>
     {{--游戏放这里--}}
