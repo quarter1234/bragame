e64946c697dee7bb5ec86b77caa90e7ba4ff1847
xdr <170882208@qq.com>
2023-09-09 18:12:01 +0800

    favor

--- resources/views/black/index/index.blade.php
+++ resources/views/black/index/index.blade.php
@@ -297,7 +297,7 @@
                           {{-- 喜欢的游戏 --}}
                           <div class="game_list">
                               @foreach($favorRecommend as $item)
-                                  <a><img _ngcontent-avh-c16="" gameid="{{ $item['id'] }}" class=" generic-background-image pg_game_go ng-star-inserted" src="{{ $item['icon'] }}" /></a>
+                                  <a><img _ngcontent-avh-c16="" gameid="{{ $item['id'] }}" class=" generic-background-image tada_game_go ng-star-inserted" src="{{ $item['icon'] }}" /></a>
                               @endforeach
                               </div>
                           </div>
