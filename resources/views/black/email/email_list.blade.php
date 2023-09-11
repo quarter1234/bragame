fb4e6d1046fe3c368e3d866a3daa08df530ecac1
unknown <you@example.com>
2023-09-11 17:24:39 +0800

    css4

--- resources/views/black/email/email_list.blade.php
+++ resources/views/black/email/email_list.blade.php
@@ -6,7 +6,7 @@
         <div class="email_xx2"></div>
         <div class="email_wk">
             <div class="email_w">Receba sua recompensa</div>
-            <div class="email_w_bottom">
+            <div class="email_w_bottom xg_bottom">
                 <div class="email_w_left">
                 <img src="https://baxigame1.s3.sa-east-1.amazonaws.com/bx_4/public/mobile/img/jb.png" />
                 {{$item['attach'][1] ?? 0}} 
