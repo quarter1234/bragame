9e89f04ab1af6bf28af43cdff5d2f1695917400a
qingtian <1043791113@qq.com>
2023-09-19 17:19:02 +0800

    123

--- resources/views/purple/pay/recharge.blade.php
+++ resources/views/purple/pay/recharge.blade.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" href="/static/css/material-icons.css">
     <link rel="stylesheet" href="/static/css/styles.4917b6f03b8811030eaf.css">
     <link rel="stylesheet" href="/static/css/DINAlternate-bold.css">
-    <link rel="stylesheet" href="/mobile/purple/css/activity.css">
-    <link rel="stylesheet" href="/mobile/purple/css/share.css">
+    <link rel="stylesheet" href="/mobile/red/css/activity.css">
+    <link rel="stylesheet" href="/mobile/red/css/share.css">
     <!-- Used in supported Android browsers -->
  
     <script>var Webconfigs = {
