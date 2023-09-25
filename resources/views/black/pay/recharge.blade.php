b2c6eaa9feb1dd301190749a97098273e593b111
xdr <170882208@qq.com>
2023-09-25 14:32:40 +0800

    点击就发起fb购物事件

--- resources/views/black/pay/recharge.blade.php
+++ resources/views/black/pay/recharge.blade.php
@@ -21,6 +21,18 @@
     <!-- Fixed position has issue with iOS Safari using black-translucent -->
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="format-detection" content="telephone=no">
+    <!-- Meta Pixel Code -->
+     <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    </script>
+    <!-- End Meta Pixel Code -->
     </head>
 
   <body style="color: white; background-color: #0a0e2b;">
@@ -152,6 +164,9 @@
       showModal('Valor inteiro positivo.');
       return false;
     }
+
+    fbq('init', '1309983866312683');
+    fbq('track', 'Purchase', {value:0.00, currency:'USD'})
 
     return true;
    }
