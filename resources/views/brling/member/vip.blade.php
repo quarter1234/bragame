1231268e085173ed02608bb0bebbced177188175
unknown <you@example.com>
2023-10-20 15:05:01 +0800

    brling

--- resources/views/brling/member/vip.blade.php
+++ resources/views/brling/member/vip.blade.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="https://bxgames3.s3.sa-east-1.amazonaws.com/bx_1/public/static/css/styles.4917b6f03b8811030eaf.css">
     <link rel="stylesheet" href="https://bxgames3.s3.sa-east-1.amazonaws.com/bx_1/public/static/css/DINAlternate-bold.css">
     <link rel="stylesheet" href="https://bxgames3.s3.sa-east-1.amazonaws.com/bx_1/public/mobile/css/activity.css">
-    <link rel="stylesheet" href="/mobile/css/member.css">
-    <link rel="stylesheet" href="/mobile/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="/mobile/brling/css/member.css">
+    <link rel="stylesheet" href="/mobile/brling/css/swiper-bundle.min.css">
     <!-- Used in supported Android browsers -->
  
     <script>var Webconfigs = {
