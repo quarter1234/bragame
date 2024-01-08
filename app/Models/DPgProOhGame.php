89d064ce4c77014fddd0b8abf1ea4399639cc038
lls <229269101@qq.com>
2024-01-08 17:45:57 +0800

    add

--- app/Models/DPgProOhGame.php
+++ app/Models/DPgProOhGame.php
@@ -9,7 +9,6 @@ class DPgProOhGame extends BaseMoel
     public function getIconAttribute($value)
     {
        // $pre = env('REMOTE_IMG_URL', 'http://localhost') . CommonEnum::S3_PATH_ARR[config('view.template')];
-        // return $pre.$value;
         return config('app.url').':82' .  $value;
         //return "https://system.777mbet.com" .  $value;
     }
